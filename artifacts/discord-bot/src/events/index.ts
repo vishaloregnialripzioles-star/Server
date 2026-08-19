0ebd71f944b464a2954de43a3a93ed8425dd12e4
vishaloregnialripzioles-star <vishaloregnialripzioles@gmail.com>
2026-08-19 16:38:18 +0530

    Register Hinglish cursed words AutoMod event

--- artifacts/discord-bot/src/events/index.ts
+++ artifacts/discord-bot/src/events/index.ts
@@ -7,6 +7,7 @@ export function registerEvents(client:Client):void{
 client.once(Events.ClientReady,safe('ready',async(...args:any[])=>{const{handleReady}=await import('./ready.js');return handleReady(...args);}));
 client.on(Events.InteractionCreate,safe('interactionCreate',async(...args:any[])=>{const{handleInteractionCreate}=await import('./interactionCreate.js');return handleInteractionCreate(...args);}));
 client.on(Events.MessageCreate,safe('messageCreate',async(...args:any[])=>{const{handleMessageCreate}=await import('./messageCreate.js');return handleMessageCreate(...args);}));
+client.on(Events.MessageCreate,safe('hinglishCursedWords',async(...args:any[])=>{const{handleHinglishCursedWords}=await import('./hinglishCursedWords.js');return handleHinglishCursedWords(...args);}));
 client.on(Events.MessageDelete,safe('messageDelete',async(...args:any[])=>{const{handleMessageDelete}=await import('./messageDelete.js');return handleMessageDelete(...args);}));
 client.on(Events.MessageUpdate,safe('messageUpdate',async(...args:any[])=>{const{handleMessageUpdate}=await import('./messageUpdate.js');return handleMessageUpdate(...args);}));
 client.on(Events.MessageReactionAdd,safe('messageReactionAdd',async(...args:any[])=>{const{handleMessageReactionAdd}=await import('./messageReactionAdd.js');return handleMessageReactionAdd(...args);}));
